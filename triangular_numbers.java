d6615d97fc1578afc3b593cb6aca68fd29026277
Petryakov_Sergey <petrykov2501@gmail.com>
2022-06-16 12:31:56 +0500

    triangular_number added

---
+++ triangular_numbers.java
@@ -0,0 +1,28 @@
+//Написать программу вычисления n-ого треугольного числа.
+
+
+import java.util.Scanner;
+
+public class triangular_numbers {
+    
+    
+    static int Tn(int n) { // с помощью рекурсии
+        if(n==1)return 1;
+        return n + Tn(n-1);
+    }
+    
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("N: ");
+        int N = iScanner.nextInt();
+        double res =0.5 * N*(N+1);
+        int value = (int) res;
+        System.out.printf("triangular_number = ");
+        System.out.println(value);
+        System.out.printf("Tn_rekur = ");
+        System.out.println(Tn(N));
+
+        iScanner.close();
+        
+    }
+}
